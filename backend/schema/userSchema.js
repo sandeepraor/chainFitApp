ce17be50812160d828f9b2ea2d79ebca6ca354a0
sandeepraor <sandeepr062003@gmail.com>
2023-12-16 15:24:29 +0530

    configuration

---
+++ backend/schema/userSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  emergency: {
+    type: String,
+    required: true,
+  },
+});
